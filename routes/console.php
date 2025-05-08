7ffbbb3887a43ded2a05a5e009192d361b6b5fef
Youri Wijnands <hey@iruoy.com>
2025-05-08 17:26:04 +0200

    update dependencies

--- routes/console.php
+++ routes/console.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command("inspire", function () {
+Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose("Display an inspiring quote");
+})->purpose('Display an inspiring quote');
 
-Schedule::command("telescope:prune")->daily();
+Schedule::command('telescope:prune')->daily();
